121cda9b198ced85810977dadf72b6a281ce20f0
chloemarion <lacymendes98@gmail.com>
2018-08-09 18:31:41 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -20,7 +20,7 @@ def iterate_through_hash
   # Implement this method so that it iterates over the hash created in add_a_key_value_pair
   # and puts each key value pair within the phrase "The _____ summer olympics took place in _____."
 summer_olympics = add_a_key_value_pair
-puts "The #{place} summer olympics took place in #{year}."
+puts "The #{place} summer olympics took place in #{year."
 return add_a_key_value_pair
 end
 
